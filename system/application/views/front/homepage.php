ae3574c3bbf5f0bb90be97c0201736b362601ecf
Thien Le <thien.le@bluecomgroup.com>
2015-11-17 19:49:46 +0700

    11/17/2015 - Home page

--- system/application/views/front/homepage.php
+++ system/application/views/front/homepage.php
@@ -1,3 +1,92 @@
 <div class="col-md-9">
-sss
+	<div class="row">
+		<div class="row-homepage service-home">
+			<div class="col-md-12 title-server-home">
+				<h3>Dịch vụ</h3>
+			</div>
+			<div class="col-md-6">
+				<div class="td-module-image">
+	                <div class="td-module-thumb">
+	                	<a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                		<img width="324" height="235" itemprop="image" class="entry-thumb td-animation-stack-type0-2" src="http://ehomesnamlong.com/wp-content/uploads/2015/11/11261691_1552663628328645_7716332463626704001_n-324x235.jpg" alt="" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                	</a>
+	                </div>
+                </div>
+            	<h3 itemprop="name" class="entry-title td-module-title"><a itemprop="url" href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">Căn hộ 3 Phòng Ngủ Ehome 3</a></h3>
+            	<div class="td-module-meta-info">
+                	<div class="td-post-author-name">
+                		<a itemprop="author" href="http://ehomesnamlong.com/author/sandatvang">Ehome 3 Tây Sài Gòn</a>
+                		<span>-</span>
+                	</div>
+                	<div class="td-post-date">
+                		<time itemprop="dateCreated" class="entry-date updated td-module-date" datetime="2015-11-03T16:29:39+00:00">03/11/2015</time>
+                		<meta itemprop="interactionCount" content="UserComments:0">
+                	</div>
+                	<div class="td-module-comments"><a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html#comments">0</a></div>
+                </div>
+	            <div class="td-excerpt">&nbsp;Căn hộ 3 Phòng Ngủ Ehome 3 có 2 loại thiết kế là 3PN-2WC và 3PN-3WC. Chủ đầu tư Nam Long muốn đem lại...</div>
+			</div>
+			<div class="col-md-6">def</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="row-homepage advices-home">
+			<div class="col-md-12 advices-home">
+				<h3>Tư vấn</h3>
+			</div>
+			<div class="col-md-6">
+				<div class="td-module-image">
+	                <div class="td-module-thumb">
+	                	<a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                		<img width="324" height="235" itemprop="image" class="entry-thumb td-animation-stack-type0-2" src="http://ehomesnamlong.com/wp-content/uploads/2015/11/11261691_1552663628328645_7716332463626704001_n-324x235.jpg" alt="" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                	</a>
+	                </div>
+                </div>
+            	<h3 itemprop="name" class="entry-title td-module-title"><a itemprop="url" href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">Căn hộ 3 Phòng Ngủ Ehome 3</a></h3>
+            	<div class="td-module-meta-info">
+                	<div class="td-post-author-name">
+                		<a itemprop="author" href="http://ehomesnamlong.com/author/sandatvang">Ehome 3 Tây Sài Gòn</a>
+                		<span>-</span>
+                	</div>
+                	<div class="td-post-date">
+                		<time itemprop="dateCreated" class="entry-date updated td-module-date" datetime="2015-11-03T16:29:39+00:00">03/11/2015</time>
+                		<meta itemprop="interactionCount" content="UserComments:0">
+                	</div>
+                	<div class="td-module-comments"><a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html#comments">0</a></div>
+                </div>
+	            <div class="td-excerpt">&nbsp;Căn hộ 3 Phòng Ngủ Ehome 3 có 2 loại thiết kế là 3PN-2WC và 3PN-3WC. Chủ đầu tư Nam Long muốn đem lại...</div>
+			</div>
+			<div class="col-md-6">def</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="row-homepage price-list-home">
+			<div class="col-md-12 price-list-home">
+				<h3>Bảng giá</h3>
+			</div>
+			<div class="col-md-6">
+				<div class="td-module-image">
+	                <div class="td-module-thumb">
+	                	<a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                		<img width="324" height="235" itemprop="image" class="entry-thumb td-animation-stack-type0-2" src="http://ehomesnamlong.com/wp-content/uploads/2015/11/11261691_1552663628328645_7716332463626704001_n-324x235.jpg" alt="" title="Căn hộ 3 Phòng Ngủ Ehome 3">
+	                	</a>
+	                </div>
+                </div>
+            	<h3 itemprop="name" class="entry-title td-module-title"><a itemprop="url" href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html" rel="bookmark" title="Căn hộ 3 Phòng Ngủ Ehome 3">Căn hộ 3 Phòng Ngủ Ehome 3</a></h3>
+            	<div class="td-module-meta-info">
+                	<div class="td-post-author-name">
+                		<a itemprop="author" href="http://ehomesnamlong.com/author/sandatvang">Ehome 3 Tây Sài Gòn</a>
+                		<span>-</span>
+                	</div>
+                	<div class="td-post-date">
+                		<time itemprop="dateCreated" class="entry-date updated td-module-date" datetime="2015-11-03T16:29:39+00:00">03/11/2015</time>
+                		<meta itemprop="interactionCount" content="UserComments:0">
+                	</div>
+                	<div class="td-module-comments"><a href="http://ehomesnamlong.com/can-ho-3-phong-ngu-ehome-3.html#comments">0</a></div>
+                </div>
+	            <div class="td-excerpt">&nbsp;Căn hộ 3 Phòng Ngủ Ehome 3 có 2 loại thiết kế là 3PN-2WC và 3PN-3WC. Chủ đầu tư Nam Long muốn đem lại...</div>
+			</div>
+			<div class="col-md-6">def</div>
+		</div>
+	</div>
 </div>
